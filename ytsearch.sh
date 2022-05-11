b989e60ef84c18e5e1e094bfa891dc203d961dab
Saphira Kai <kai.saphira@gmail.com>
2022-05-11 18:05:55 -0300

    use a function to update search results asynchronously

--- ytsearch.sh
+++ ytsearch.sh
@@ -17,6 +17,23 @@ update_search () {
 	cat /tmp/ytsearch-search_data.json | jq -r '.[].title' > /tmp/ytsearch-titles
 }
 
+get_unique_results () {
+	declare -A memory
+
+	set +u
+	while :; do
+		[ -e /tmp/ytsearch-titles ] && {
+			for title in $(cat /tmp/ytsearch-titles 2>/dev/null); do
+				[[ ${memory["$title"]} ]] || {
+					memory["$title"]=1
+					echo "$title"
+				}
+			done
+		} || exit
+	done
+	set -u
+}
+
 output=title
 
 [ "${1:-}" = new_query ] && {
@@ -38,7 +55,12 @@ touch /tmp/ytsearch-titles
 fzf_options=('--prompt' 'youtube search: '
              '--reverse'
              '--bind' 'change:execute-silent(ytsearch new_query {q} &)'
-             '--bind' 'change:+reload(cat /tmp/ytsearch-titles)')
-: | fzf ${fzf_options[@]}
+             #'--bind' 'change:+reload(cat /tmp/ytsearch-titles)'
+             )
+get_unique_results | fzf ${fzf_options[@]} &
+fzf_pid=$!
 
-rm /tmp/ytsearch-search_data.json /tmp/ytsearch-titles
+while ps -p $fzf_pid >/dev/null; do sleep 0.1; done
+
+rm -f /tmp/ytsearch-search_data.json /tmp/ytsearch-titles
+exit 0
